cb836a6448628aa7f1980736888420456f406a5d
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2022-07-08 10:37:39 +0300

    string interpolation & binding properties & events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './routes/app-routing.module';
+import { AppComponent } from './root/app.component';
+import { StringInterpolationComponent } from './components/string-interpolation/string-interpolation.component';
+import { BindingPropertiesComponent } from './components/binding-properties/binding-properties.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, StringInterpolationComponent, BindingPropertiesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
